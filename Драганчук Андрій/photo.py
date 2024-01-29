146c825b1ac806bf9df922e26a9f094fd4ad869c
Andriy37 <131591248+Andriy37@users.noreply.github.com>
2024-01-29 17:57:10 +0200

    Add files via upload

--- Драганчук Андрій/photo.py
+++ Драганчук Андрій/photo.py
@@ -1,7 +1,8 @@
 from PyQt5.QtCore import Qt
+import os
 from PyQt5.QtWidgets import (
     QButtonGroup, QLineEdit, QGroupBox, QLabel, QApplication, QWidget,
-    QListWidget, QPushButton, QVBoxLayout, QRadioButton, QHBoxLayout, QMessageBox, QSpinBox
+    QListWidget, QPushButton, QVBoxLayout, QRadioButton, QHBoxLayout, QMessageBox, QSpinBox, QFileDialog,
 )
 from PIL import ImageFilter, Image
 
@@ -16,7 +17,7 @@ btn_vpravo = QPushButton("Вправо")
 btn_dzerkalo = QPushButton("Дзеркало")
 btn_rizkist = QPushButton("Різкість")
 btn_niga = QPushButton("Ч/Б")
-btn_panel = QListWidget()
+panel = QListWidget()
 photo = QLabel()
 
 line4 = QHBoxLayout()
@@ -25,21 +26,45 @@ line1 = QVBoxLayout()
 line3 = QVBoxLayout()
 
 line1.addWidget(btn_dir)
-line1.addWidget(btn_panel)
+line1.addWidget(panel)
 line3.addWidget(photo)
 line2.addWidget(btn_vlivo)
 line2.addWidget(btn_vpravo)
 line2.addWidget(btn_dzerkalo)
 line2.addWidget(btn_rizkist)
-line2.addWidget(btn_vlivo)
-
+line2.addWidget(btn_niga)
 
 line3.addLayout(line2)
 
-line4.addLayout(line1, 80)
-line4.addLayout(line3, 20)
+line4.addLayout(line1, 20)
+line4.addLayout(line3, 80)
 
 win.setLayout(line4)  # Додано цей рядок для встановлення макету вікна
+
+workdir = ""
+
+def chooseWorkdir():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+
+def filter(files, extensions):
+    result = []
+    for file_name in files:
+        for ext in extensions:
+            if file_name.endswith(ext):
+                result.append(file_name)
+    return result
+
+def showFilenameList():
+    global workdir
+    extensions = ['.jpg','.jpeg','.png','.gif']
+    chooseWorkdir()
+    file_names = filter(os.listdir(workdir), extensions)
+    panel.clear()
+    for file_name in file_names:
+        panel.addItem(file_name)
+
+btn_dir.clicked.connect(showFilenameList)
 
 win.show()
 app.exec_()
